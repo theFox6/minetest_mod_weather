7124aaaf05fa6cb3a8d215483f244467dbd2efde
khonkhortisan <khonkhortisan@gmail.com>
2013-09-07 09:51:59 -0700

    Vertical rain

--- weather/rain.lua
+++ weather/rain.lua
@@ -13,12 +13,12 @@ minetest.register_globalstep(function(dtime)
 		local vel = {x=0, y=   -4, z=0}
 		local acc = {x=0, y=-9.81, z=0}
 
-		minetest.add_particlespawner(25, 0.5,
-			minp, maxp,
-			vel, vel,
-			acc, acc,
-			0.8, 0.8,
-			25, 25,
-			false, "weather_rain.png", player:get_player_name())
+		minetest.add_particlespawner({amount=25, time=0.5,
+			minpos=minp, maxpos=maxp,
+			minvel=vel, maxvel=vel,
+			minacc=acc, maxacc=acc,
+			minexptime=0.8, maxexptime=0.8,
+			minsize=25, maxsize=25,
+			collisiondetection=false, vertical=true, texture="weather_rain.png", player=player:get_player_name()})
 	end
 end)
